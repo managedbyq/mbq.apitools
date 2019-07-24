b9884679e82069022c43db73355d156e5b21054f
dursk <mmadurski@managedbyq.com>
2019-07-24 13:42:14 -0400

    test and version bump

--- mbq/api_tools/fields.py
+++ mbq/api_tools/fields.py
@@ -24,7 +24,7 @@ class Field(fields.Field):
             return super().__new__(cls)
 
         elif kwargs.pop("many", False):
-            transform = kwargs.pop("transform")
+            transform = kwargs.pop("transform", None)
             return _List(cls_or_instance=cls(transform=transform, **kwargs), **kwargs)
 
         return super().__new__(cls)
