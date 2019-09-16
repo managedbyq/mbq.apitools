f62a50ce0f5461fadc8d0bf6136f1ef50f2365e9
Per-Andre Stromhaug <pstromhaug@managedbyq.com>
2019-09-15 20:30:30 -0400

    wip

--- mbq/api_tools/qviews.py
+++ mbq/api_tools/qviews.py
@@ -1,20 +1,41 @@
-from typing import TypeVar, cast
+from typing import TypeVar, cast, Type
 from django.http import HttpRequest, JsonResponse
+from importlib import import_module
 
 T = TypeVar('T')
 
 
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class QSchema:
+    pass
+
+
+def _construct_payload(payload: dict, schema_class: Type[QSchema]) -> QSchema:
+    return schema_class(**payload)
+
+
 def qview(func: T) -> T:
-    def wrapper(*args, **kwargs):
-        return func(*args, **kwargs)
+    def wrapper(request, *args, **kwargs):
+        schema_class = func.__annotations__["payload"]
+        print('schema_class', schema_class)
+        the_payload = _construct_payload(request, schema_class)
+        return func(request, the_payload, *args, **kwargs)
 
-    return cast(T, wrapper())
+    return cast(T, wrapper)
 
 
+@dataclass(frozen=True)
+class TestSchema(QSchema):
+    a: int
+    b: str
 
 @qview
-def test_func(request: HttpRequest, order_id: int) -> JsonResponse:
-    return JsonResponse('abc')
+def test_func(request: dict, payload: TestSchema, order_id: int) -> str:
+    print("in test_func payload", request, payload)
+    return 'abc'
 
 
-reveal_type(test_func)
+test_func(request={'a': 1, 'b': 'b'}, order_id=1)
